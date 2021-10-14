182d3b0ae878e5e44881e79bdcc3be0912229400
Samantha Laine <samanthalaine8@gmail.com>
2021-10-14 18:39:13 -0400

    initial commit

---
+++ be-well-backend/routes/auth.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const User = require('../models/User.js')
+
+
+//Register
+router.post('/register', async (req, res) => {
+
+})
+
+module.exports = router
