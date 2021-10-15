36865a24af5e8f763996b8b5cbc049cacf3787d4
Samantha Laine <samanthalaine8@gmail.com>
2021-10-15 17:05:37 -0400

    added unfollow feature

--- be-well-backend/routes/users.js
+++ be-well-backend/routes/users.js
@@ -80,4 +80,24 @@ router.put('/:id/follow', async (req, res)=>{
 
 //unfollow a user
 
+router.put('/:id/unfollow', async (req, res)=>{
+  if (req.body.userId !== req.params.id){
+    try {
+      const user = await User.findById(req.params.id)
+      const currentUser = await User.findById(req.body.userId)
+      if (  user.followers.includes(req.body.userId)){
+        await user.updateOne({$pull:{followers: req.body.userId}})
+        await currentUser.updateOne({$pull:{following: req.params.id}})
+        res.status(200).json("User has been unfollowed.")
+      } else {
+        res.status(403).json("You don't follow this person.")
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You cannot unfollow yourself");
+  }
+});
+
 module.exports = router 
