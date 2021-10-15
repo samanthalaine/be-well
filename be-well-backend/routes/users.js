05371841d731c34c00589f838e5f20da8c1657f0
Samantha Laine <samanthalaine8@gmail.com>
2021-10-15 14:13:47 -0400

    added follow user feature

--- be-well-backend/routes/users.js
+++ be-well-backend/routes/users.js
@@ -47,13 +47,36 @@ router.delete("/:id", async (req, res) => {
 router.get('/:id', async (req, res)=>{
   try {
     const user = await User.findById(req.params.id)
-    res.status(200).json(user);
+    const {password, updatedAt, ...other} = user._doc
+    res.status(200).json(other);
   } catch (err) {
     res.status(500).json(err)
   }
 })
 
-//follow a username
+//follow a user
+
+router.put('/:id/follow', async (req, res)=>{
+  if (req.body.userId !== req.params.id){
+    try {
+      const user = await User.findById(req.params.id)
+      const currentUser = await User.findById(req.body.userId)
+      if (!user.followers.includes(req.body.userId)){
+        await user.updateOne({$push:{followers: req.body.userId}})
+        await currentUser.updateOne({$push:{following: req.params.id}})
+        res.status(200).json("User has been followed.")
+      } else {
+        res.status(403).json("You already follow this person.")
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You cannot follow yourself");
+  }
+});
+
+
 
 //unfollow a user
 
