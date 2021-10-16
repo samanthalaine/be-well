10f3cb1ec707e8a76869141b5018a53d85e16783
Samantha Laine <samanthalaine8@gmail.com>
2021-10-16 16:24:23 -0400

    added create a post feature

--- be-well-backend/routes/posts.js
+++ be-well-backend/routes/posts.js
@@ -1,7 +1,26 @@
 const router = require('express').Router()
+const Post = require('../models/Post')
 
-router.get("/", (req, res) => {
-    console.log("Post page!")
+
+//create post
+
+router.post('/', async (req, res) => {
+    const newPost = Post(req.body)
+    try{
+        const savedPost = await newPost.save()
+        res.status(200).json(savedPost)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
+
+
+
+
+//update post
+//delete post
+//like a post
+//get a post
+//get timeline posts
 
 module.exports = router
