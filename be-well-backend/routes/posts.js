d1f13ff8422341e487e0972238a8c46d331111d4
Samantha Laine <samanthalaine8@gmail.com>
2021-10-16 17:00:47 -0400

    added delete post feature

--- be-well-backend/routes/posts.js
+++ be-well-backend/routes/posts.js
@@ -31,10 +31,23 @@ router.put('/:id', async (req, res) => {
     }
 })
 
-
-
-
 //delete post
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id)
+        if (post.userId === req.body.userId){
+            await post.deleteOne()
+            res.status(200).json("Post has been deleted")
+        } else {
+            res.status(403).json("You can only delete your own post.")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
 //like a post
 //get a post
 //get timeline posts
