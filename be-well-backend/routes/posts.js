3dce09b07ff63baf9a5d5332269cab74cf1336eb
Samantha Laine <samanthalaine8@gmail.com>
2021-10-16 16:54:53 -0400

    update post feature

--- be-well-backend/routes/posts.js
+++ be-well-backend/routes/posts.js
@@ -15,9 +15,25 @@ router.post('/', async (req, res) => {
 })
 
 
-
-
 //update post
+
+router.put('/:id', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id)
+        if (post.userId === req.body.userId){
+            await post.updateOne({$set:req.body})
+            res.status(200).json("Post has been updated")
+        } else {
+            res.status(403).json("You can only update your own post.")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
+
 //delete post
 //like a post
 //get a post
