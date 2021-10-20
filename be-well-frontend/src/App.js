96ce4ef5ddfde3d3420bde9a674955537535c28f
Samantha Laine <samanthalaine8@gmail.com>
2021-10-19 20:36:11 -0400

    react router

--- be-well-frontend/src/App.js
+++ be-well-frontend/src/App.js
@@ -1,11 +1,33 @@
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-  //return <Home/>
-  //return <Login/>
-  return <Register/>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path = '/'>
+          <Home/>
+        </Route>
+        <Route path = '/login'>
+          <Login/>
+        </Route>
+        <Route path = '/register'>
+          <Register/>
+        </Route>
+        {/* <Route path = '/profile/:username'>
+          <Profile/>
+        </Route> */}
+      </Switch>
+    </Router>
+  )
+  
 }
 
 export default App;
