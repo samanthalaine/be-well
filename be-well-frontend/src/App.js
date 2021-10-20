2cab87ccbcb562c3968ccde8105f8e3d4dec1cc9
Samantha Laine <samanthalaine8@gmail.com>
2021-10-19 20:15:42 -0400

    login and register ui

--- be-well-frontend/src/App.js
+++ be-well-frontend/src/App.js
@@ -1,7 +1,11 @@
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
-  return <Home/>
+  //return <Home/>
+  //return <Login/>
+  return <Register/>
 }
 
 export default App;
