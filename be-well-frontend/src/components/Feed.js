e2f0bec0b961ba5dc7ae7aabf4b32be309473e86
Samantha Laine <samanthalaine8@gmail.com>
2021-10-19 21:52:39 -0400

    proxy

--- be-well-frontend/src/components/Feed.js
+++ be-well-frontend/src/components/Feed.js
@@ -1,20 +1,31 @@
 import "./feed.css";
-import {Posts} from "./dummyData"
+//import {Posts} from "./dummyData"
 import Post from './Post'
 import Share from './Share'
-
+import { useState, useEffect } from 'react'
 import React from 'react'
+import axios from "axios";
 
 function Feed() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() =>{
+        const fetchPosts = async () => {
+
+        const res = await axios.get('posts/timeline/616b47b8fe9bd3d17ce9145b')
+        console.log(res)
+        }
+        fetchPosts()
+    }, [])
+
     return (
         <>
-        
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
-                {Posts.map((post)=>(
+                {/* {Posts.map((post)=>(
                     <Post key={post.id} post={post}/>
-                ))}
+                ))} */}
             </div>
             
         </div>
