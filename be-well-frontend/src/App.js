3f8230293e2fb00dfdfefa34213410de4d36eaab
Samantha Laine <samanthalaine8@gmail.com>
2021-10-25 11:10:01 -0400

    fixed routes

--- be-well-frontend/src/App.js
+++ be-well-frontend/src/App.js
@@ -21,18 +21,15 @@ function App() {
 
     <Router>
       <Switch>
-        <Route exact path = '/'>
-        {user ? <Home /> : <Register />}
-          
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
         </Route>
-        <Route path = '/login'>
-          <Login/>
-        </Route>
-        <Route path = '/register'>
-          <Register/>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
         </Route>
         <Route path="/profile/:username">
-          <Profile/>
+          <Profile />
         </Route>
       </Switch>
     </Router>
