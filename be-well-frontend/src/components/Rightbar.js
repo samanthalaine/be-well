78b67e4cd4b34544af21c1e4c303b28d69e4fb89
Samantha Laine <samanthalaine8@gmail.com>
2021-10-25 03:09:37 -0400

    added expandable card back to rightbar

--- be-well-frontend/src/components/Rightbar.js
+++ be-well-frontend/src/components/Rightbar.js
@@ -1,250 +1,26 @@
-// import "./rightbar.css";
-// import "./feed.css";
-// import Post from "./Post";
-// import React from "react";
-// import Online from "./Online";
-// import {
-//   Button,
-//   Card,
-//   CardActionArea,
-//   CardActions,
-//   CardContent,
-//   CardMedia,
-//   makeStyles,
-//   Typography,
-//   IconButton,
-//   CardHeader,
-//   Collapse,
-//   Avatar,
-//   Container,
-//   styled,
-// } from "@material-ui/core";
 
-// import FavoriteIcon from "@mui/icons-material/Favorite";
-// import ShareIcon from "@mui/icons-material/Share";
-// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-// import MoreVertIcon from "@mui/icons-material/MoreVert";
-// import { red } from "@mui/material/colors";
-// import { AuthContext } from "../context/AuthContext";
-// import { useContext, useEffect, useState } from "react";
-// import { Add, Remove } from "@material-ui/icons";
-// import axios from "axios";
-
-
-
-
-// const useStyles = makeStyles((theme) => ({
-//   toolbar: theme.mixins.toolbar,
-//   container: {
-//     paddingTop: theme.spacing(10),
-//     position: "sticky",
-//     top: 0,
-//   },
-//   card: {
-//     marginBottom: theme.spacing(5),
-//   },
-//   media: {
-//     height: 250,
-//     [theme.breakpoints.down("sm")]: {
-//       height: 150,
-//     },
-//     content: {
-//       flexGrow: 1,
-//       backgroundColor: theme.palette.background.default,
-//       padding: theme.spacing(3),
-//     },
-//   },
-// }));
-
-// //expandable card from https://mui.com/components/cards/
-
-// const ExpandMore = styled((props) => {
-//   const { expand, ...other } = props;
-//   return <IconButton {...other} />;
-// })(({ theme, expand }) => ({
-//   transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-//   marginLeft: "auto",
-//   transition: theme.transitions.create("transform", {
-//     duration: theme.transitions.duration.shortest,
-//   }),
-// }));
-
-// const Rightbar = ({user}) => {
-//      const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
-//     const [friends, setFriends] = useState([]);
-//     const {user: currentUser, dispatch} = useContext(AuthContext);
-//     const [followed, setFollowed] = useState(false);
-    
-//     useEffect(() => {
-//         setFollowed(currentUser.following.includes(user?._id));
-//     }, [currentUser, user?._id]);
-
-
-//   function HomeRightbar() {
-//     const classes = useStyles();
-//     const [expanded, setExpanded] = React.useState(false);
-
-//     const handleExpandClick = () => {
-//       setExpanded(!expanded);
-//     };
-
-//     return (
-//       <main className={classes.content}>
-//         <div className={classes.toolbar} />
-//         <Container
-//           className={classes.container}
-//           style={{ display: "flex", width: "400px" }}
-//         >
-//           <Card className={classes.card}>
-//             <CardHeader
-//               avatar={
-//                 <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-//                   R
-//                 </Avatar>
-//               }
-//               action={
-//                 <IconButton aria-label="settings">
-//                   <MoreVertIcon />
-//                 </IconButton>
-//               }
-//               title="Recipe of the Week"
-//               subheader="Shrimp and Chorizo Paella"
-//             />
-//             <CardMedia
-//               className={classes.media}
-//               component="img"
-//               height="194"
-//               image="https://media.istockphoto.com/photos/spicy-homemade-cajun-jambalaya-picture-id537314539?b=1&k=20&m=537314539&s=170667a&w=0&h=VcDpFMQHmRShi5Ole1QIVw5I5MiT5rw9k4uxRd3o1Gk="
-//               alt="Paella dish"
-//             />
-//             <CardContent>
-//               <Typography variant="body2" color="text.secondary">
-//                 This impressive paella is a perfect party dish and a fun meal to
-//                 cook together with your guests. Add 1 cup of frozen peas along
-//                 with the mussels, if you like.
-//               </Typography>
-//             </CardContent>
-//             <CardActions disableSpacing>
-//               <IconButton aria-label="add to favorites">
-//                 <FavoriteIcon />
-//               </IconButton>
-//               <IconButton aria-label="share">
-//                 <ShareIcon />
-//               </IconButton>
-//               <ExpandMore
-//                 expand={expanded}
-//                 onClick={handleExpandClick}
-//                 aria-expanded={expanded}
-//                 aria-label="show more"
-//               >
-//                 <ExpandMoreIcon />
-//               </ExpandMore>
-//             </CardActions>
-//             <Collapse in={expanded} timeout="auto" unmountOnExit>
-//               <CardContent>
-//                 <Typography paragraph>Method:</Typography>
-//                 <Typography paragraph>
-//                   Heat 1/2 cup of the broth in a pot until simmering, add saffron
-//                   and set aside for 10 minutes.
-//                 </Typography>
-//                 <Typography paragraph>
-//                   Heat oil in a (14- to 16-inch) paella pan or a large, deep
-//                   skillet over medium-high heat. Add chicken, shrimp and chorizo,
-//                   and cook, stirring occasionally until lightly browned, 6 to 8
-//                   minutes. Transfer shrimp to a large plate and set aside, leaving
-//                   chicken and chorizo in the pan. Add pimentón, bay leaves,
-//                   garlic, tomatoes, onion, salt and pepper, and cook, stirring
-//                   often until thickened and fragrant, about 10 minutes. Add
-//                   saffron broth and remaining 4 1/2 cups chicken broth; bring to a
-//                   boil.
-//                 </Typography>
-//                 <Typography paragraph>
-//                   Add rice and stir very gently to distribute. Top with artichokes
-//                   and peppers, and cook without stirring, until most of the liquid
-//                   is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
-//                   reserved shrimp and mussels, tucking them down into the rice,
-//                   and cook again without stirring, until mussels have opened and
-//                   rice is just tender, 5 to 7 minutes more. (Discard any mussels
-//                   that don’t open.)
-//                 </Typography>
-//                 <Typography>
-//                   Set aside off of the heat to let rest for 10 minutes, and then
-//                   serve.
-//                 </Typography>
-//               </CardContent>
-//             </Collapse>
-//           </Card>
-//         </Container>
-//       </main>
-//     );
-//   }
-
-//   const handleClick = async () => {
-//     try {
-//       if (followed) {
-//         await axios.put(`/users/${user._id}/unfollow`, {
-//           userId: currentUser._id,
-//         });
-//         dispatch({ type: "UNFOLLOW", payload: user._id });
-//       } else {
-//         await axios.put(`/users/${user._id}/follow`, {
-//           userId: currentUser._id,
-//         });
-//         dispatch({ type: "FOLLOW", payload: user._id });
-//       }
-//     } catch (err) {
-//       console.log(err);
-//     }
-//     setFollowed(!followed)
-//   };
-
-//   const ProfileRightbar = () => {
-//     return (
-//       <>
-//        {user.username !== currentUser.username && (
-//           <button className="rightbarFollowButton" onClick={handleClick}>
-//             {followed ? "Unfollow" : "Follow"}
-//             {followed ? <Remove /> : <Add />}
-//           </button>
-//         )}
-//         <h4 className="rightbarTitle">Details</h4>
-//         <div className="rightbarInfo">
-//           <div className="rightbarInfoItem">
-//             <span className="rightbarInfoKey">City:</span>
-//             <span className="rightbarInfoValue">New York</span>
-//           </div>
-//           <div className="rightbarInfoItem">
-//             <span className="rightbarInfoKey">Country:</span>
-//             <span className="rightbarInfoValue">USA</span>
-//           </div>
-          
-//         </div>
-//         <h4 className="rightbarTitle">Following</h4>
-//         <div className="rightbarFollowings">
-//           <div className="rightbarFollowing">
-//             <img
-//               src="assets/person/1.jpeg"
-//               alt="friend"
-//               className="rightbarFollowingImg"
-//             />
-//             <span className="rightbarFollowingName">John Carter</span>
-//           </div>          
-//         </div>
-//       </>
-//     );
-//   };
-
-//   return (
-//     <div className="rightbar">
-//       <div className="rightbarWrapper">
-//         {user ? <ProfileRightbar /> : <HomeRightbar />}
-//       </div>
-//     </div>
-//   );
-
-// }
-
-// export default Rightbar;
+import React from "react";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  makeStyles,
+  Typography,
+  IconButton,
+  CardHeader,
+  Collapse,
+  Avatar,
+  Container,
+  styled,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { red } from "@mui/material/colors";
 
 import "./rightbar.css";
 import { Users } from "./dummyData";
@@ -295,25 +71,138 @@ export default function Rightbar({ user  }) {
     }
   };
 
-  const HomeRightbar = () => {
+  const useStyles = makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+  container: {
+    paddingTop: theme.spacing(10),
+    position: "sticky",
+    top: 0,
+  },
+  card: {
+    marginBottom: theme.spacing(5),
+  },
+  media: {
+    height: 250,
+    [theme.breakpoints.down("sm")]: {
+      height: 150,
+    },
+    content: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.default,
+      padding: theme.spacing(3),
+    },
+  },
+}));
+
+  const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+    function HomeRightbar() {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+
     return (
-      <>
-        <div className="birthdayContainer">
-          <img className="birthdayImg" src="assets/gift.png" alt="" />
-          <span className="birthdayText">
-            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
-          </span>
-        </div>
-        <img className="rightbarAd" src="assets/ad.png" alt="" />
-        <h4 className="rightbarTitle">Online Friends</h4>
-        <ul className="rightbarFriendList">
-          {/* {Users.map((u) => (
-            <Online key={u.id} user={u} />
-          ))} */}
-        </ul>
-      </>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Container
+          className={classes.container}
+          style={{ display: "flex", width: "400px" }}
+        >
+          <Card className={classes.card}>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  R
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title="Recipe of the Week"
+              subheader="Shrimp and Chorizo Paella"
+            />
+            <CardMedia
+              className={classes.media}
+              component="img"
+              height="194"
+              image="https://media.istockphoto.com/photos/spicy-homemade-cajun-jambalaya-picture-id537314539?b=1&k=20&m=537314539&s=170667a&w=0&h=VcDpFMQHmRShi5Ole1QIVw5I5MiT5rw9k4uxRd3o1Gk="
+              alt="Paella dish"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to
+                cook together with your guests. Add 1 cup of frozen peas along
+                with the mussels, if you like.
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
+              <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Method:</Typography>
+                <Typography paragraph>
+                  Heat 1/2 cup of the broth in a pot until simmering, add saffron
+                  and set aside for 10 minutes.
+                </Typography>
+                <Typography paragraph>
+                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
+                  skillet over medium-high heat. Add chicken, shrimp and chorizo,
+                  and cook, stirring occasionally until lightly browned, 6 to 8
+                  minutes. Transfer shrimp to a large plate and set aside, leaving
+                  chicken and chorizo in the pan. Add pimentón, bay leaves,
+                  garlic, tomatoes, onion, salt and pepper, and cook, stirring
+                  often until thickened and fragrant, about 10 minutes. Add
+                  saffron broth and remaining 4 1/2 cups chicken broth; bring to a
+                  boil.
+                </Typography>
+                <Typography paragraph>
+                  Add rice and stir very gently to distribute. Top with artichokes
+                  and peppers, and cook without stirring, until most of the liquid
+                  is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+                  reserved shrimp and mussels, tucking them down into the rice,
+                  and cook again without stirring, until mussels have opened and
+                  rice is just tender, 5 to 7 minutes more. (Discard any mussels
+                  that don’t open.)
+                </Typography>
+                <Typography>
+                  Set aside off of the heat to let rest for 10 minutes, and then
+                  serve.
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </Container>
+      </main>
     );
-  };
+  }
 
   const ProfileRightbar = () => {
     return (
